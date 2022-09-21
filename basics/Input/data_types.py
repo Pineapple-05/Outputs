5783ff4042228b06b604779942f2a75562aabcfb
Pineapple-05 <6BOSSE81@solent.ac.uk>
2022-09-21 14:28:40 +0100

    Activity 3
    Added code to demonstrate the use of different data types.

---
+++ basics/Input/data_types.py
@@ -0,0 +1,11 @@
+name = input("What is your name human?")
+
+age = int (input("How old are you (in years)?"))
+
+height = float (input("How tall are you (in meters)?"))
+
+weight= int (input("How much do you weigh (in kilograms)?"))
+
+bmi = round(weight/(height ** 2),2)
+
+print(f"{name} you are {age} years old and your bmi is {bmi}")
