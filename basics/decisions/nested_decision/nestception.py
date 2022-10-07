03de5c9ef79c0ee8896f99742e714b09b3c5027f
Pineapple-05 <6BOSSE81@solent.ac.uk>
2022-10-07 12:31:22 +0100

    Activity 2 improved
    Added code to demonstrate the use of multiple nested statements."

--- basics/decisions/nested_decision/nestception.py
+++ basics/decisions/nested_decision/nestception.py
@@ -6,19 +6,19 @@ if location == "in the bedroom" :
     else:
         print("Found some mess but no battery.")
 
-if location == "in the bathroom" :
+elif location == "in the bathroom" :
     location2 = input("Where in the bathroom should I look?")
     if location2 == "in the bathtub":
         print("Found a rubber duck but no battery")
     else:
         print("Found a wet surface but no battery.")
 
-if location == "in the lab" :
+elif location == "in the lab" :
     location2 = input("Where in the lab should I look?")
     if location2 == "on the table":
         print("Yes! I found my battery!")
     else:
         print("Found some tools but no battery.")
 
-if location != "in the lab" and location != "in the bedroom" and location != "in the bathroom":
+else:
     print("I do not know where that is but I will keep looking.")
