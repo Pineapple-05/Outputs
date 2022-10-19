2d57cb5ce98e68419bb7fd9597f50a1b4424523c
Pineapple-05 <6BOSSE81@solent.ac.uk>
2022-10-19 13:51:09 +0100

    Ascii_characters
    Added code to demonstrate the use of built-in functions: abs, range, chr

---
+++ basics/functions/ascii_characters.py
@@ -0,0 +1,8 @@
+print("Program Started!")
+code = int(input("Please enter an ASCII code:"))
+if code in range(32, 127):
+    letter = ord(code)
+    print(f"The character represented by the ASCII code {code} is: {letter} ")
+else:
+    print("please restart programme choosing suitable number with in the range 32-126")
+    
