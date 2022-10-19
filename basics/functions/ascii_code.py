7966557dca065cb360cb6734877afe160287acc9
Pineapple-05 <6BOSSE81@solent.ac.uk>
2022-10-19 13:27:24 +0100

    ascii functions
    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,6 @@
+print("Program Started!")
+letter = input("Please enter a letter:")
+if len(letter) == 1:
+    num = ord(letter)
+print(f"The ASCII code for {letter} is: {num}")
+print("Program Ended!")
