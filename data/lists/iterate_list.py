6c6d6d99535886c58dcf59c779f7d28c6bedfd53
Pineapple-05 <6BOSSE81@solent.ac.uk>
2022-11-29 11:15:27 +0000

    push

---
+++ data/lists/iterate_list.py
@@ -0,0 +1,18 @@
+def directions():
+    directions_ = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions_
+
+
+def menu():
+    print("Please select a direction:")
+    directions_ = directions()
+    for i in range(len(directions_)):
+        direction = directions_[i]
+        print(f"{direction}: {i}")
+
+
+def run():
+    menu()
+
+run()
+
